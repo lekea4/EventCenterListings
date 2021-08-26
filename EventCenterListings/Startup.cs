c704b64db9504fcd2476effce4a5c416d81a5bf2
Adeleke Ayinde <lekea4@gmail.com>
2021-08-26 01:57:25 +0100

    make changes to configure method in startup.cs

--- EventCenterListings/Startup.cs
+++ EventCenterListings/Startup.cs
@@ -30,7 +30,7 @@ namespace EventCenterListings
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EventCenterListings", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Event Centers", Version = "v1" });
             });
         }
 
@@ -40,9 +40,14 @@ namespace EventCenterListings
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventCenterListings v1"));
+                
             }
+
+            //removed the first two functions from the configure method to make the documentation available at all times regardless if when in development or production
+           
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventCenterListings v1"));
 
             app.UseHttpsRedirection();
 
